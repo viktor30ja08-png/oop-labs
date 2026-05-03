b9c716073d6288276577748ece7cb459210ab97a
viktor30ja08-png <viktor30ja08@gmail.com>
2026-05-03 22:46:55 +0300

    func changes

--- ooplab/lab1_oop_test.py
+++ ooplab/lab1_oop_test.py
@@ -1,5 +1,5 @@
 import unittest
-from main import validate_matrix, multiply_matrix_by_scalar, get_sum_of_min_in_columns
+from ooplab.lab1_oop import validate_matrix, multiply_matrix_by_scalar, get_sum_of_min_in_columns
 
 
 class TestMatrixOperations(unittest.TestCase):
