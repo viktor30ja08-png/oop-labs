e47216872154d887c9f1a15aeba9633a7643e163
viktor30ja08-png <viktor30ja08@gmail.com>
2026-05-05 00:42:07 +0300

    lab5 changes

--- ooplab5f/lab5_oop.py
+++ ooplab5f/lab5_oop.py
@@ -112,7 +112,7 @@ class Playroom:
 def main():
     try:
 
-        room = Playroom(budget=200.00)
+        room = Playroom(budget=100.00)
 
         small_car = Car("Hot Wheels Fast", 15.50, 3, 7, "small")
         large_car = Car("RC Monster Truck", 45.00, 8, 14, "large")
